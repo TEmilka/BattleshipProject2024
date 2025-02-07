a69afe4d6fb42e2b6f2dcc153e66d6eae0d98cb3
TEmilka <emilkatrybala@gmail.com>
2025-02-07 16:48:40 +0100

    Fixed main

--- CppCLR_WinFormsProject.cpp
+++ CppCLR_WinFormsProject.cpp
@@ -1,15 +1,13 @@
 #include "pch.h"
 #include "Form1.h"
 
-using namespace System::Windows::Forms;
+using namespace System;
 
-[STAThread]
-int main()
+
+int main(array<System::String^>^ args)
 {
- Application::EnableVisualStyles();
- Application::SetCompatibleTextRenderingDefault(false);
- Application::Run(gcnew CppCLRWinFormsProject::Form1());
-
-
-  return 0;
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew CppCLRWinFormsProject::Form1(args));
+    return 0;
 }
